b3f3ce50241121ce213ad3692c3170caedf18109
Bryan Capitulo <bryancapitulo@gmail.com>
2020-03-10 17:42:24 +0000

    Automatically backed up by Learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -6,10 +6,9 @@
 #suite_e: "Crumpet the Elf"}
 
 def select_winner(passengers)
-  winner = " "
+  winner = ""
   passengers.each do |suite, name|
-    if suite == :suite_a && name.start_with?("A")
-      winner = name
+    if suite == :suite_a && name.start_with?("A") winner = name
     end
   end
 end
